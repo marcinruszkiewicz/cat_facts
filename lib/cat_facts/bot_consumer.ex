56f41a1b819af537fb9e2b188d2ce5f95db36d71
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2023-08-31 11:55:04 +0200

    slash command for admins only

--- lib/cat_facts/bot_consumer.ex
+++ lib/cat_facts/bot_consumer.ex
@@ -17,8 +17,24 @@ defmodule CatFacts.BotConsumer do
     end
   end
 
-  def handle_event({:READY, _event, _state}) do
-    IO.puts "Cat Facts are Ready"
+  def handle_event({:READY, info, _state}) do
+    command = %{
+      name: "fact",
+      description: "Add a cat fact.",
+      default_member_permissions: 8, # admin
+      options: [
+        %{
+          type: 3, # string
+          name: "content",
+          description: "Cat fact to be added.",
+          required: true
+        }
+      ]
+    }
+
+    Enum.each(info.guilds, fn guild ->
+      Api.bulk_overwrite_guild_application_commands(guild.id, [command])
+    end)
   end
 
   def handle_event(_), do: :noop
